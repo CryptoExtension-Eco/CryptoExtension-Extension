5c59bc6df675abc470e017ceae2bbdf01e3a3359
Julien <62757437+NapoTwiixe306@users.noreply.github.com>
2023-03-01 19:33:42 +0100

    extension

--- cryptoextension/src/App.test.js
+++ cryptoextension/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import Popup from '../main/popup/Popup';
+import App from './App';
 
 test('renders learn react link', () => {
-  render(<Popup />);
+  render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
