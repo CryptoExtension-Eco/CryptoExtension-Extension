eeceb234b5b264d931a58d7e3dbb751eb57ed896
napotwiixe <julien.milants@gmail.com>
2023-05-20 12:18:55 +0200

    d

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer class="footer"> 
-    <p class="footer__title">CryptoExtension</p>
-    <p className='copy'>&#169; <span id="date"></span> copyright all right reserved</p>
-</footer>
+        <p class="footer__title">CryptoExtension</p>
+        <p className='copy'>&#169; <span id="date"></span> copyright all right reserved</p>
+    </footer>
   );
 };
 
