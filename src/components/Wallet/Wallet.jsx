89d4b8e085384bde4773f29e6570d974dabb5583
napotwiixe <julien.milants@gmail.com>
2023-05-24 20:52:30 +0200

    add card design

--- src/components/Wallet/Wallet.jsx
+++ src/components/Wallet/Wallet.jsx
@@ -6,18 +6,21 @@ import visa from '../img/visa.png'
 const Wallet = () => {
   return (
     <>
-        <div id="wallet"/>
-            <div className="wallet" id='Wallet'>
-              <p id="userWallet" className='text-lg text-gray-600 my-2 ethAdresse'></p>
-              <div className="card1">
-                <img className='chip' src={chip} alt="" />
-                <img className='contactless' src={contactLess} alt="" />
-                <img className='visa' src={visa} alt="" />
-                <div className="arrow"></div>
-                <p className="card-name"></p>
-                <p className="bank-name"></p>
-              </div>
-            </div>
+         <div className="wallet" id="Wallet">
+                    <p id="userWallet" className="text-lg text-gray-600 my-2 ethAdresse"></p>
+                    <div className="card-container">
+                        <div className="card1">
+                            <img className="chip" src={chip}alt=""/>
+                            <img className="contactless" src={contactLess} alt=""/>
+                            <img className="visa" src={visa} alt="" />
+                            <p id="test" className="card-number">tqt</p>
+                            <div className="arrow">
+                            </div>
+                            <p className="card-name">Ethereum</p>
+                            <p className="bank-name">CryptoBank</p>
+                        </div>
+                    </div>
+                </div>
     </>
   )
 }
